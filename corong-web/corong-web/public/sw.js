1c6fe86944c31c7d6cf820bd3df445cf5a489aac
Irlandosimanjorang <simanjorangirlando@gmail.com>
2026-08-21 22:42:12 +0700

    Create sw.js

---
+++ corong-web/corong-web/public/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", () => {
+  // Passthrough - biarin browser handle fetch-nya normal, gak di-intercept/cache.
+});
