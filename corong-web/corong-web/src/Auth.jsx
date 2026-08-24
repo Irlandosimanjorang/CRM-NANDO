5560967405570a12dc744db5120ead3d2eea4bcd
Irlandosimanjorang <simanjorangirlando@gmail.com>
2026-08-24 22:41:31 +0700

    Update Auth.jsx

--- corong-web/corong-web/src/Auth.jsx
+++ corong-web/corong-web/src/Auth.jsx
@@ -36,6 +36,10 @@ function NextoLogo({ width = 90, className = "" }) {
       alt="Nexto"
       width={width}
       className={`block h-auto shrink-0 object-contain ${className}`}
+      style={{
+        filter:
+          "contrast(1.35) brightness(0.72) drop-shadow(0 1px 2px rgba(15, 23, 42, 0.12))",
+      }}
     />
   );
 }
